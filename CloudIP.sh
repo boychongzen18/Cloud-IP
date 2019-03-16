a623b77db5ae02c94a9974ab63baa6519af76e67
boychongzen <38745550+boychongzen18@users.noreply.github.com>
2019-03-16 09:40:20 +0700

    Add files via upload

---
+++ CloudIP.sh
@@ -0,0 +1,19 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+pkg update && pkg upgrade
+pkg install -y wget python unrar figlet
+echo
+echo "Cloud-IP Downloading GoogleDrive......"
+echo
+wget ' https://drive.google.com/uc?id=1QhdSCLmWvx4hdgfqm_4FRdgbnOoBSbP-&export=download' -O Cloud-IP.rar
+echo
+echo "extracting file ........"
+echo
+unrar x Cloud-IP.rar Cloud-IP
+echo
+echo " Deleted Fie Rar...... "
+echo
+rm Cloud-IP.rar
+echo 
+mv Cloud-IP $HOME
+echo
